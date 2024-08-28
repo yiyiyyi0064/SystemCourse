9f0921d5b05a76d60606793ed030a0dfc3f49171
tracer-NEMU2020 <tjuics2020@163.com>
2024-08-28 11:14:48 +0800

    >  compile NEMU 3023244340 yiyiyyi Linux LAPTOP-6GAUKPRI 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:14:48 up  4:59,  1 user,  load average: 0.09, 0.05, 0.01 ad5d46cedaa2597c1e7849f21b8ee232fbfb8dd

--- NEMU2021/nemu/src/monitor/debug/ui.c
+++ NEMU2021/nemu/src/monitor/debug/ui.c
@@ -36,6 +36,13 @@ static int cmd_q(char *args) {
 	return -1;
 }
 
+static int cmd_si(char *args) {
+	char* num_exe=strtok(args," ");
+	int num_exed=atoi(num_exe);
+	cpu_exec(num_exed);
+	return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -46,8 +53,8 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
 	/* TODO: Add more commands */
+	{"si","Single execuation",cmd_si},
 
 };
 
