34cfea7d0a1667d62e38474482b66cc00df2d154
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-03 23:18:55 +0800

    >  compile NEMU 3023244340 yiyiyyi Linux LAPTOP-6GAUKPRI 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:18:55 up 1 day, 23:48,  1 user,  load average: 0.08, 0.02, 0.01 b6b57b6c771b7bad38e5bb5f4ed17a7ef62679

--- NEMU2021/nemu/src/monitor/debug/expr.c
+++ NEMU2021/nemu/src/monitor/debug/expr.c
@@ -194,7 +194,7 @@ uint32_t expr(char *e, bool *success) {
 		return 0;
 	}
 	/* TODO: Insert codes to evaluate the expression. */
-	return eval(0,nr_token-1);
+	return eval(0,nr_token);
 
 }
 int domi_position(int p,int q){	
