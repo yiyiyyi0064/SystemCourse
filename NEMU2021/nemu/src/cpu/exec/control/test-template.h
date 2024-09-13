f3f6f45daf225477fcf363cbf0319151387a32cf
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-13 15:47:34 +0800

    >  compile NEMU 3023244340 yiyiyyi Linux LAPTOP-6GAUKPRI 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:47:34 up  6:06,  1 user,  load average: 0.00, 0.00, 0.00 73d56651901822cb58102c2980b0ef871cf61a03

--- NEMU2021/nemu/src/cpu/exec/control/test-template.h
+++ NEMU2021/nemu/src/cpu/exec/control/test-template.h
@@ -7,6 +7,7 @@ static void do_execute() {
   //执行AND操作决定标志位
   //op_dest op_src 目标操作数与源操作数 
   cpu.eflags.SF = ret >> ((DATA_BYTE << 3) - 1); 
+  //((DATA_BYTE << 3) - 1) 将databyte转化为以位为单位 
   //取结果的最高位 表示结果的符号
   cpu.eflags.ZF = !ret; 
   //如果ret==0 那么zf=1
@@ -14,9 +15,15 @@ static void do_execute() {
   cpu.eflags.OF = 0; 
   //test指令不影响这两个
   ret ^= ret >> 4;
+  //将高四位与第四位一起折叠，确定整个低八位的1的奇偶
   ret ^= ret >> 2;
   ret ^= ret >> 1;
-  ret &= 1; // if last bit == 1, odd number
+  ret &= 1; 
+  //逐步折叠的操作！！！
+  //折叠过后相当于这八位全都进行过组合 因为我的目标是数奇偶 那必然遍历。
+  //又因为：偶数个1会产生0 奇数个1会产生1
+  //故最后结果为1 则是奇数
+  //pf 如果最低字节中1的个数是偶数则设置为1
   cpu.eflags.PF = !ret; // PF == 1, even number, Pf == 0, odd number
   DATA_TYPE result = op_dest -> val & op_src -> val;
   update_eflags_pf_zf_sf((DATA_TYPE_S)result);
