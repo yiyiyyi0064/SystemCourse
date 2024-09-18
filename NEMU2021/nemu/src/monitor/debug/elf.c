ca90ee5b7dd847a2e45480de996b83d864fe51a5
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-18 09:59:07 +0800

    >  compile NEMU 3023244340 yiyiyyi Linux LAPTOP-6GAUKPRI 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:59:07 up  4:39,  1 user,  load average: 0.00, 0.00, 0.00 c7a612ae4896e4d2b92496876b4c5d0bf457647

--- NEMU2021/nemu/src/monitor/debug/elf.c
+++ NEMU2021/nemu/src/monitor/debug/elf.c
@@ -108,7 +108,7 @@ uint32_t getValue(char* str,bool* success){
             //字符串表+符号偏移量 = 符号所在地址STT_FUNC代表符号的类型是一个函数（符号的类型在低四位）
 			if (strcmp(strtab + symtab[i].st_name, str) == 0){ 
 				//return symtab[i].st_value;
-			printf("%d\n",symtab[i].st_value);
+			printf("%x\n",symtab[i].st_value);
 			return 0;
 		}
 	}
